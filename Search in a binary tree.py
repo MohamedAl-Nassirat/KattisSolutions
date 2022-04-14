45420e4317fc621d5ff62bcdc82bd1c6d91e604c
Mohamed Al-Nassirat <malnassi@ualberta.ca>
2022-04-14 14:01:37 -0600

    Leetcode BST

---
+++ Search in a binary tree.py
@@ -0,0 +1,13 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # first check if val is in the node 
+        if self.val = int:
+        
+        else: 
+            return null
